02a336f6626adf226593d0b424acd5fa5a52479f
mariano <marianomartinez84@gmail.com>
2018-05-21 11:30:53 -0300

    I add deployment files

--- deploy-files/gunicorn_execute.sh
+++ deploy-files/gunicorn_execute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # IMPORTANT: This file must be placed next to manage.py
 source /home/russ/venv-russ2018/bin/activate
-exec /home/russ/venv-russ2018/bin/gunicorn -c /home/russ/russia-live-2018-backend/Russia2018Backend/gunicorn.conf.py chekin.wsgi:application
+exec /home/russ/venv-russ2018/bin/gunicorn -c /home/russ/russia-live-2018-backend/Russia2018Backend/gunicorn.conf.py Russia2018Backend.wsgi:application
