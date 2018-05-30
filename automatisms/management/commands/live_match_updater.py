e454d39861471ba95c4bb30819945fb1782f0724
mariano <marianomartinez84@gmail.com>
2018-05-30 19:41:53 -0300

    lineups added

--- automatisms/management/commands/live_match_updater.py
+++ automatisms/management/commands/live_match_updater.py
@@ -215,8 +215,8 @@ def update_match_lineups_from_json(match, lineups_json):
     # Team 1 is always local and Team 2 is always visitor
     team1_id = int(match.team1.external_id)
     team2_id = int(match.team2.external_id)
-    team1_players = [('%3s' % p['number']) + ('. %s' % p['player_name']) for p in lineups_json if p['team_id'] == team1_id]
-    team2_players = [('%3s' % p['number']) + ('. %s' % p['player_name']) for p in lineups_json if p['team_id'] == team2_id]
+    team1_players = [('%4s' % p['number']) + ('. %s' % p['player_name']) for p in lineups_json if p['team_id'] == team1_id]
+    team2_players = [('%4s' % p['number']) + ('. %s' % p['player_name']) for p in lineups_json if p['team_id'] == team2_id]
     match.team1_lineup = team1_players
     match.team2_lineup = team2_players
     match.save()
